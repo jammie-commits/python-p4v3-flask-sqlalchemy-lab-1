3d1805f6532dbfc574813e1f800be720f45c8e4d
jay.mbugua.ph@gmail.com <jammie@DESKTOP-CDCBFL2.>
2024-07-05 17:33:22 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -7,14 +7,7 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
-# Edit server/models.py to add new model class named Earthquake that inherits from both db.Model and SerializerMixin
-# Add the following attributes to the Earthquake model:
 
-# a string named __tablename__ assigned to the value "earthquakes".
-# a column named id to store an int that is the primary key.
-# a column named magnitude to store a float.
-# a column named location to store a string.
-# A column named year to store an int.
 
 class Earthquake(db.Model, SerializerMixin):
     __tablename__ = "earthquakes"
@@ -22,9 +15,6 @@ class Earthquake(db.Model, SerializerMixin):
     magnitude = db.Column(db.Float)
     location = db.Column(db.String)
     year = db.Column(db.Integer)
-
-# Add a __repr__ method to return a string that formats the attributes id, magnitude, location, and year as a comma-separated 
-# string, e.g. 1, 9.5, Chile, 1960
 
     def __repr__(self):
         return f"{self.id}, {self.magnitude}, {self.location}, {self.year}"
